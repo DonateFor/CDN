2049f764b42e866e039f6662993393d84899a8f0
DonateFor <donatefor237@gmail.com>
2024-10-23 17:30:34 +0530

    Update jcn.js

--- jcn.js
+++ jcn.js
@@ -1,6 +1,6 @@
 var amont = "true"; 
-const provider = "@ybl";
-const upiname = "";
+const provider = "@ptys";
+const upiname = "paytmqr5hl7lp";
 const upid = upiname + provider ;
 const adminname = "DonateFor";
 // triger point "true"
